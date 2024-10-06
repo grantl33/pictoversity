9cdd1a513c7f8ee62b6de81b50506770e46b0059
Grant Lee <grantlee@Grants-MacBook-Pro.local>
2024-10-05 22:33:35 -0700

    fixes

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,7 +4,7 @@ import posterImage from "../../assets/video_poster.png";
 function Hero(props) {
     return (
         <div className="hero">
-            <video width="100%" poster={posterImage} controls>
+            <video poster={posterImage} controls>
                 <source src={introvideo} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
